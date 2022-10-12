40738444b7f3116fe470e6c0ba485a8c16e9379a
achoora <achoora@users.noreply.github.com>
2022-10-12 13:46:35 +0200

    simple methods to reverse a list

---
+++ list_reversal.py
@@ -0,0 +1,11 @@
+import numpy as np
+import array
+print ('Hello World')
+my_list =["Hello","Mr","Perera"]
+print (my_list)
+result=my_list[::-1]
+print (result)
+result.reverse()
+print(result)
+rresult=list(reversed(result))
+print(rresult)
